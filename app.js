2f8bdb636fa0fb5e30f73af031ed5e30eb0a8345
kanikamital0606 <kanikamittal0661996@gmail.com>
2021-06-21 12:55:17 +0530

    added some middleware

--- app.js
+++ app.js
@@ -1,8 +1,33 @@
 const express = require('express');
 
 const app = express();
-app.use('/', (req, res)=>{
+
+//**********************Add other piece of middleware****************************
+//the first logs "Request received" to the console, and hands on execution
+app.use((req,res,next)=> {
+    console.log('Request recieved !!');
+    next();
+});
+
+//**********************Add other piece of middleware****************************
+//**********************Add http status code ****************************
+//the second adds a 201 status code to the response, and hands on execution
+
+app.use('/', (req, res,next)=>{
+    res.status(201);
+    next();
+});
+
+//the third sends the JSON response, and hands on execution
+app.use('/', (req, res, next)=>{
     res.json({message: 'Your request was sucessful'});
+    next();
 })
+
+//**********************Add other piece of middleware****************************
+//the final piece of middleware logs "Response sent successfully" to the console
+app.use((req,res,next)=>{
+    console.log('Reponse sent Successfully !!')
+});
 
 module.exports = app;
